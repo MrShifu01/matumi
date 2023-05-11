093185b14e08451f8bad845fb8595428f204df37
ChristianStander <stander.christian@gmail.com>
2023-05-11 17:40:37 +0200

    set up react integration

---
+++ matumi-site/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import '../styles.css'
+
+export default function Navigation () {
+    return (
+        <div className='navigation container'>
+            <nav className='navbar'>
+                <ul className='nav-list'>
+                    <li className='nav-item'>Home</li>
+                    <li className='nav-item'>About</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
